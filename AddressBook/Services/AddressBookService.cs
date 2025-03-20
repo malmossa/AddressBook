95d2883746f2cd360225cb4cece28673ec3f44e6
malmossa <malmossa@gmail.com>
2025-03-19 19:40:14 -0700

    completed

--- AddressBook/Services/AddressBookService.cs
+++ AddressBook/Services/AddressBookService.cs
@@ -13,9 +13,28 @@ namespace AddressBook.Services
         {
             _context = context;
         }
-        public Task AddContactToCategoryAsync(int categoryId, int contactId)
+        public async Task AddContactToCategoryAsync(int categoryId, int contactId)
         {
-            throw new NotImplementedException();
+            try
+            {
+                // Check to see if the category is un the contact
+                if (!await IsContactInCategory(categoryId, contactId))
+                {
+                    Contact? contact = await _context.Contacts.FindAsync(contactId);
+                    Category? category = await _context.Categories.FindAsync(contactId);
+
+                    if (category != null && contact != null)
+                    {
+                        category.Contacts.Add(contact);
+                        await _context.SaveChangesAsync();
+                    }
+                }
+
+            }
+            catch
+            {
+                throw;
+            }
         }
 
         public Task<ICollection<Category>> GetContactCategoriesAsync(int contactId)
@@ -46,9 +65,14 @@ namespace AddressBook.Services
             return categories;
         }
 
-        public Task<bool> IsContactInCategory(int categoryId, int contactId)
+        public async Task<bool> IsContactInCategory(int categoryId, int contactId)
         {
-            throw new NotImplementedException();
+            Contact? contact = await _context.Contacts.FindAsync(contactId);
+
+            return await _context.Categories
+                                 .Include(c => c.Contacts)
+                                 .Where(c => c.Id == categoryId && c.Contacts.Contains(contact))
+                                 .AnyAsync();
         }
 
         public Task RemoveContactFromCategoryAsync(int categoryId, int contactId)
