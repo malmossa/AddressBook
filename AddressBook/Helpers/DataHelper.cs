ce8eced9da35110b66097fa80ea939fa380e3228
malmossa <malmossa@gmail.com>
2025-04-03 08:55:06 -0700

    completed

---
+++ AddressBook/Helpers/DataHelper.cs
@@ -0,0 +1,17 @@
+﻿using AddressBook.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace AddressBook.Helpers
+{
+    public static class DataHelper
+    {
+        public static async Task ManageDataAsync(IServiceProvider svcProvider)
+        {
+            //Get an instance of the db application context
+            var dbContextSvc = svcProvider.GetRequiredService<ApplicationDbContext>();
+
+            //Mugration: this is equivalent to update-database
+            await dbContextSvc.Database.MigrateAsync();
+        }
+    }
+}
